1f42907520542bb9a496de8fae9ef27c9455dea0
cjhorton <jayhorton@gmail.com>
2025-05-29 07:13:43 -0400

    Install Chakra UI, dependencies, and snippets per recommendations.  Update default Chakra snippets to pass ESLint errors.  Clear default App.css and index.css.  Add a color mode button in App to ensure color mode works after changes.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AppProvider } from "@/components/ui/app-provider.tsx";
 
 const rootElement = document.getElementById('root');
 
@@ -11,6 +12,8 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
     <StrictMode>
-        <App />
+        <AppProvider>
+            <App/>
+        </AppProvider>
     </StrictMode>
 );
