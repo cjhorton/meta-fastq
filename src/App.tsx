13938ae212a04d543139b3918ab1dcc7efbbf78a
cjhorton <jayhorton@gmail.com>
2025-05-30 09:51:53 -0400

    Add the Action Bar component to App and wire in actions and statuses with placeholders.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import { ColorModeButton } from "@/components/ui/color-mode";
 import { FastqUploader } from "@/components/fastq-uploader.tsx";
 import type { FileRejection } from "@/types/file-upload-types";
 import { RejectedList } from "@/components/rejected-list.tsx";
+import { ActionBar } from "@/components/action-bar.tsx";
+import { type Action, type EnabledActions, getEnabledActions } from "@/types/action.ts";
+import type { Status as AppStatus } from "@/types/status.ts";
 
 function App() {
+    const [status, setStatus] = useState<AppStatus>('Idle')
     const [rejectedFiles, setRejectedFiles] = useState<FileRejection[]>([]);
     const [acceptedFiles, setAcceptedFiles] = useState<File[]>([]);
+    const [enabledActions, setEnabledActions] = useState<EnabledActions>(getEnabledActions('Idle'));
 
     const handleFileAccept = (files: File[]) => {
         setAcceptedFiles(files);
@@ -17,6 +22,54 @@ function App() {
         setRejectedFiles(files);
     };
 
+    const handleRunAction = () => {
+        updateStatus('Running');
+    };
+
+    const handleClearAction = () => {
+        //TODO: clear files
+        console.log('clear files');
+    };
+
+    const handleResetAction = () => {
+        //TODO: reset app
+        console.log('reset app');
+    };
+
+    const handleSaveAction = () => {
+        //TODO: save results
+        console.log('save results');
+    };
+
+    const handleUserAction = (action: Action) => {
+        switch (action) {
+            case 'Run':
+                handleRunAction();
+                break;
+            case 'Clear':
+                handleClearAction();
+                break;
+            case 'Reset':
+                handleResetAction();
+                break;
+            case 'Save':
+                handleSaveAction();
+                break;
+        }
+    };
+
+    const updateStatus = (newStatus: AppStatus) => {
+        if (newStatus !== status) {
+            setStatus(newStatus);
+            updateEnabledActions(newStatus);
+        }
+    };
+
+    const updateEnabledActions = (status: AppStatus) => {
+        const enabledActions = getEnabledActions(status);
+        setEnabledActions(enabledActions);
+    };
+
     return (
         <Stack align="flex-start" w="full">
             <ColorModeButton/>
@@ -24,6 +77,7 @@ function App() {
             <Show when={rejectedFiles.length > 0}>
                 <RejectedList rejection={rejectedFiles}/>
             </Show>
+            <ActionBar enabledActions={enabledActions} onActionSelected={handleUserAction}></ActionBar>
             <FastqUploader
                 showUpload={true}
                 setAcceptedFiles={handleFileAccept}
