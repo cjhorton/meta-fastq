af279cbc9799b2872243dbc1fc4c5b9d15a99cda
cjhorton <jayhorton@gmail.com>
2025-05-30 09:35:55 -0400

    Create the status type.  Create the status-action map for the action bar button states.  Create the getEnabledActions convenience function.

--- src/types/action.ts
+++ src/types/action.ts
@@ -1,3 +1,5 @@
+import type { Status } from "./status";
+
 export type Action = 'Clear' | 'Run' | 'Reset' | 'Save';
 
 export interface EnabledActions {
@@ -5,4 +7,42 @@ export interface EnabledActions {
     run: boolean;
     reset: boolean;
     save: boolean;
+}
+
+const statusActionMap: Record<Status, EnabledActions> = {
+    Idle: {
+        clear: false,
+        run: false,
+        reset: false,
+        save: false,
+    },
+    Pending: {
+
+        clear: true,
+        run: true,
+        reset: false,
+        save: false,
+    },
+    Running: {
+        clear: false,
+        run: false,
+        reset: true,
+        save: false,
+    },
+    Complete: {
+        clear: false,
+        run: false,
+        reset: true,
+        save: true,
+    },
+    Error: {
+        clear: false,
+        run: false,
+        reset: true,
+        save: false,
+    },
+}
+
+export function getEnabledActions(status: Status): EnabledActions {
+    return statusActionMap[status];
 }
