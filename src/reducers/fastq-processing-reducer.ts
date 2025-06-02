0871779554049f3318239f4261c1f54e36cdfdb8
cjhorton <jayhorton@gmail.com>
2025-06-02 15:34:53 -0400

    Implement the initial processing reducer.

---
+++ src/reducers/fastq-processing-reducer.ts
@@ -0,0 +1,19 @@
+import type { FastqResult } from "@/types/fastq-result.ts";
+
+export type Action =
+    | { type: 'initialize'; files: File[] }
+    | { type: 'setResult'; result: FastqResult }
+    | { type: 'setError'; fileName: string };
+
+export function fastqProcessingReducer(state: FastqResult[], action: Action): FastqResult[] {
+    switch (action.type) {
+        case 'initialize':
+            return action.files.map(f => ({file: f, status: 'Pending'}));
+        case 'setResult':
+            return state.map(r => r.file.name === action.result.file.name ? action.result : r);
+        case 'setError':
+            return state.map(r => r.file.name === action.fileName ? {...r, status: 'Error'} : r);
+        default:
+            return state;
+    }
+}
