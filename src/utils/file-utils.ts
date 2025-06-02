b353350d2acd76dc4d9954a5361cc6b18abb5b3e
cjhorton <jayhorton@gmail.com>
2025-06-02 12:38:42 -0400

    Create a function to generate a human-readable file size to be used in the UI.

--- src/utils/file-utils.ts
+++ src/utils/file-utils.ts
@@ -34,3 +34,37 @@ export function isFastqFileQuick(file: File): boolean {
 export function areSameFileQuick(file1: File, file2: File): boolean {
     return file1.name === file2.name;
 }
+
+const KB = 1024;
+const MB = KB * 1024;
+const GB = MB * 1024;
+
+/**
+ * Formats a file size in bytes into a human-readable string with appropriate units (B, KB, MB, GB).
+ * This represents the size on disk
+ *
+ * @param size - The size of the file in bytes. Must be a non-negative number.
+ * @return A formatted string representing the file size in the most appropriate unit.
+ */
+export function formatFileSize(size: number): string {
+    if (!size || size < 0) {
+        return '0 B';
+    }
+
+    if (size >= GB) {
+        const value = (size / GB).toFixed(size >= 10 * GB ? 1 : 2);
+        return `${String(value)} GB`;
+    }
+
+    if (size >= MB) {
+        const value = (size / MB).toFixed(size >= 10 * MB ? 1 : 2);
+        return `${String(value)} MB`;
+    }
+
+    if (size >= KB) {
+        const value = (size / KB).toFixed(size >= 10 * KB ? 1 : 2);
+        return `${String(value)} KB`;
+    }
+
+    return `${String(size)} B`;
+}
