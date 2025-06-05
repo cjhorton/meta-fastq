5013aa73494ebf03d48569dad4bfb3a81b2ce4da
cjhorton <jayhorton@gmail.com>
2025-06-05 14:23:42 -0400

    Add a utility function to create an Illumina result.

---
+++ src/utils/result-utils/illumina-result-utils.ts
@@ -0,0 +1,24 @@
+import { BASES_LINE, type FastqRead, HEADER_LINE } from "@/types/fastq-types.ts";
+import { parseIlluminaHeader } from "@/utils/header-utils/illumina-header-utils.ts";
+import type { FastqResult } from "@/types/fastq-result.ts";
+
+export const createIlluminaResult = (file: File, firstRead: FastqRead): FastqResult => {
+    const header = parseIlluminaHeader(firstRead[HEADER_LINE]);
+
+    if (!header) {
+        return {
+            file,
+            status: 'Error',
+            error: 'Expected Illumina header',
+            platform: 'Unknown'
+        };
+    }
+
+    return {
+        file,
+        status: 'Done',
+        platform: 'Illumina',
+        runNumber: header.runNumber,
+        cycles: firstRead[BASES_LINE].length,
+    };
+}
