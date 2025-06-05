f44f9eb80c171cbfe7a79e28e9becb13d0423283
cjhorton <jayhorton@gmail.com>
2025-06-05 14:18:05 -0400

    Add a utility function to determine the platform from a header.

---
+++ src/utils/header-utils/fastq-platform-utils.ts
@@ -0,0 +1,10 @@
+import type { PlatformType } from "@/types/platform.ts";
+import { isIlluminaPlatform } from "@/utils/header-utils/illumina-header-utils.ts";
+
+export const determinePlatform = (header: string): PlatformType => {
+    if (!header) return 'Unknown';
+
+    if (isIlluminaPlatform(header)) return 'Illumina';
+
+    return 'Unknown';
+}
