06d024cd7912bac3422fdfc1d9466ed4f3a5fb67
cjhorton <jayhorton@gmail.com>
2025-06-05 17:35:58 -0400

    Add table columns for new results fields.

--- src/components/fastq-results-table.tsx
+++ src/components/fastq-results-table.tsx
@@ -1,7 +1,7 @@
 import { Table } from "@chakra-ui/react"
 import type { FastqResult } from "@/types/fastq-result.ts";
 import { formatFileSize } from "@/utils/file-utils.ts";
-import { valueOrDash } from "@/utils/ui-utils.ts";
+import { valueOrDefault } from "@/utils/ui-utils.ts";
 
 export const FastqResultsTable = ({results}: { results: FastqResult[] }) => {
     return (
@@ -10,23 +10,31 @@ export const FastqResultsTable = ({results}: { results: FastqResult[] }) => {
                 <Table.Row>
                     <Table.ColumnHeader>Name</Table.ColumnHeader>
                     <Table.ColumnHeader>Status</Table.ColumnHeader>
-                    <Table.ColumnHeader>Size</Table.ColumnHeader>
+                    <Table.ColumnHeader>Error Msg</Table.ColumnHeader>
+                    <Table.ColumnHeader>Size (Disk)</Table.ColumnHeader>
                     <Table.ColumnHeader>Platform</Table.ColumnHeader>
+                    <Table.ColumnHeader>Instrument Id</Table.ColumnHeader>
+                    <Table.ColumnHeader>Flow Cell Id</Table.ColumnHeader>
+                    <Table.ColumnHeader>Read</Table.ColumnHeader>
                     <Table.ColumnHeader>Cycles</Table.ColumnHeader>
                     <Table.ColumnHeader>Indexes/Sample #</Table.ColumnHeader>
                 </Table.Row>
             </Table.Header>
             <Table.Body>
                 {results.map((result) => {
-                    const {file, status, platform, cycles, indexes} = result;
+                    const {file, status, error, platform, instrumentId, flowcellId, readNumber,cycles, indexes} = result;
                     return (
                         <Table.Row key={file.name}>
                             <Table.Cell>{file.name}</Table.Cell>
                             <Table.Cell>{status}</Table.Cell>
+                            <Table.Cell>{valueOrDefault(error, 'N/A')}</Table.Cell>
                             <Table.Cell>{formatFileSize(file.size)}</Table.Cell>
-                            <Table.Cell>{valueOrDash(platform)}</Table.Cell>
-                            <Table.Cell>{valueOrDash(cycles)}</Table.Cell>
-                            <Table.Cell>{valueOrDash(indexes)}</Table.Cell>
+                            <Table.Cell>{valueOrDefault(platform)}</Table.Cell>
+                            <Table.Cell>{valueOrDefault(instrumentId)}</Table.Cell>
+                            <Table.Cell>{valueOrDefault(flowcellId)}</Table.Cell>
+                            <Table.Cell>{valueOrDefault(readNumber)}</Table.Cell>
+                            <Table.Cell>{valueOrDefault(cycles)}</Table.Cell>
+                            <Table.Cell>{valueOrDefault(indexes)}</Table.Cell>
                         </Table.Row>
                     );
                 })}
