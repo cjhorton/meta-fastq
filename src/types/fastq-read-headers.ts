f33608a9eaea2d054f0621ef656fe8956964d844
cjhorton <jayhorton@gmail.com>
2025-06-05 14:16:47 -0400

    Add Illumina and Nanopore header types.

---
+++ src/types/fastq-read-headers.ts
@@ -0,0 +1,29 @@
+export interface IlluminaHeader {
+    instrument: string;
+    runNumber: number;
+    flowcellId: string;
+    lane: number;
+    tile: number;
+    xPos: number;
+    yPos: number;
+    umi?: string;
+    read: number;
+    isFiltered: boolean;
+    control: number;
+    index: string;
+}
+
+export interface NanoporeHeader {
+    readId: string;
+    runId?: string;
+    sampleId?: string;
+    flowCellId?: string;
+    protocolGroupId?: string;
+    channel?: number;
+    startTime?: string;
+    basecallModelVersionId?: string;
+    basecallGpu?: string;
+    barcode?: string;
+    barcodeAlias?: string;
+    parentReadId?: string;
+}
