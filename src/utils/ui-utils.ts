32797c21539323c2395a748c145c24d8e071ece5
cjhorton <jayhorton@gmail.com>
2025-06-06 12:08:39 -0400

    Add a function to format an array for UI or set a default value.

--- src/utils/ui-utils.ts
+++ src/utils/ui-utils.ts
@@ -1,2 +1,11 @@
 export const valueOrDefault = (
     value: string | number | null | undefined, defaultValue = '-'): string => String(value ?? defaultValue);
+
+export const arrayOrDefault = (
+    value: (string | number)[] | null | undefined,
+    defaultValue = '-'): string => {
+    if (value == null || value.length === 0) {
+        return defaultValue;
+    }
+    return value.join(', ');
+};
