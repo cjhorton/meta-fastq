2e2b1bcd74015dae0c3449ebcdcedc8982f789c5
cjhorton <jayhorton@gmail.com>
2025-06-10 17:02:19 -0400

    Rename the fastq results table and place it in the component directory.  Rework logic to use the table columns.

---
+++ src/components/FastqResultsTable/FastqResultsTable.tsx
@@ -0,0 +1,30 @@
+import { Table } from "@chakra-ui/react"
+import type { FastqResult } from "@/types/fastq-result.ts";
+import { Tooltip } from "@/components/ui/tooltip";
+import { tableColumns } from "./table-columns.ts";
+
+export const FastqResultsTable = ({results}: { results: FastqResult[] }) => {
+    return (
+        <Table.Root size="sm">
+            <Table.Header>
+                <Table.Row>
+                    {tableColumns.map((column) => (
+                        <Tooltip key={column.key} content={column.tooltipText} openDelay={500}>
+                            <Table.ColumnHeader>{column.label}</Table.ColumnHeader>
+                        </Tooltip>
+                    ))}
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {results.map((result) => (
+                    <Table.Row key={result.file.name}>
+                        {tableColumns.map((column) => (
+                            <Table.Cell key={column.key}>{column.accessor(result)}
+                            </Table.Cell>
+                        ))}
+                    </Table.Row>
+                ))}
+            </Table.Body>
+        </Table.Root>
+    );
+}
