7fa84406ed4af5f8b5675f486722f84657db41a8
cjhorton <jayhorton@gmail.com>
2025-06-16 14:53:04 -0400

    Add nanopore platform determination.

--- src/utils/header-utils/fastq-platform-utils.ts
+++ src/utils/header-utils/fastq-platform-utils.ts
@@ -1,10 +1,13 @@
 import type { PlatformType } from "@/types/platform.ts";
 import { isIlluminaPlatform } from "../../utils/header-utils/illumina-header-utils.ts";
+import { isNanoporePlatform } from "../../utils/header-utils/nanopore-header-utils.ts";
 
 export const determinePlatform = (header: string): PlatformType => {
     if (!header) return 'Unknown';
 
     if (isIlluminaPlatform(header)) return 'Illumina';
+
+    if (isNanoporePlatform(header)) return 'Nanopore';
 
     return 'Unknown';
 }
